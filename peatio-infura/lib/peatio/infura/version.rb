7db613b6f63aa576bda67424bc575e85b5d6da36
iman <irwebuniq@gmail.com>
2020-07-11 01:23:17 +0430

    Fix some bugs

--- peatio-infura/lib/peatio/infura/version.rb
+++ peatio-infura/lib/peatio/infura/version.rb
@@ -1,5 +1,5 @@
 module Peatio
   module Infura
-    VERSION = '0.1.5'
+    VERSION = '0.1.61'
   end
 end
--- peatio-infura/test.rb
+++
@@ -1,5 +0,0 @@
-require "lib/peatio/infura"
-
-print("00000")
-client = Peatio::Infura::Client.new(client = Peatio::Infura::Client.new("https://mainnet.infura.io/v3/186a22c7f0fe4dff998d57a823085ce4","https://mainnet.infura.io/v3/186a22c7f0fe4dff998d57a823085ce4"))
-client.json_rpc(:eth_blockNumber)
